dc7c19ff8dd861dfdd3937a8799a61ef58ada370
Anthony Risch <rischthefisch@gmail.com>
2020-01-23 03:10:29 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,7 +1,3 @@
-def introduction_with_language(name, language)
-  puts "Hi, my name is #{name} and i am learning to prgram with #{language}."
-end
-
 def introduction(name)
   puts "hi, my name is #{name}."
 end
