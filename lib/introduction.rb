c8cb42f1b8521a907ef11e682baed0e64efca46d
Anthony Risch <rischthefisch@gmail.com>
2020-01-23 03:14:39 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -6,4 +6,5 @@ def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and i am learning to program in #{language}."
 end
 
+introduction("Dan")
 introduction_with_language("Dan", "Ember.js")
